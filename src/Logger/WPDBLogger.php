8ddd1ae5d8be53e2eb2fb67009eb0c4353c2f494
Ayesh Karunaratne <ayesh@ayesh.me>
2020-04-20 20:07:46 +0700

    Implement WPDBLogger with write queue

--- src/Logger/WPDBLogger.php
+++ src/Logger/WPDBLogger.php
@@ -7,17 +7,40 @@ use wpdb;
 
 class WPDBLogger implements LoggerInterface {
 
+    /**
+     * @var wpdb
+     */
     private $database;
+
+    private $write_queue = [];
 
     public function __construct(wpdb $database) {
         $this->database;
     }
 
     public function commit(): void {
-        // TODO: Implement commit() method.
+        if (!$this->write_queue) {
+            return;
+        }
+
+        foreach ($this->write_queue as $entry) {
+            // todo: write in a multi-insert query.
+            $this->database->insert('error_log', [
+                'timestamp' => $entry->timestamp,
+                'severity' => $entry->severity,
+                'url' => substr($entry->url, -32000),
+                'referrer' => substr($entry->url, -32000),
+                'hostname' => substr($entry->url, -128),
+                'error_type' => substr($entry->url, -128),
+                'error_message' => $entry->error_message,
+                'error_vars' => $entry !== null
+                    ? json_encode($entry->error_vars)
+                    : null
+            ]);
+        }
     }
 
     public function log(LogEntry $entry): void {
-        var_dump($entry);
+        $this->write_queue[] = $entry;
     }
 }
